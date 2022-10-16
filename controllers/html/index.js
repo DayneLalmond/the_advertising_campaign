422d33bdc02d95de344d0fdfb92a20c03e45cffd
Joyce Chen <joyceideas@outlook.com>
2022-10-16 14:44:12 -0400

    added handlebars

--- controllers/html/index.js
+++ controllers/html/index.js
@@ -1,9 +1,30 @@
 const router = require("express").Router();
 router.get("/", async (req, res) => {
-    return res.render("home")
+  return res.render("home", {
+    layout: "main",
+  });
 });
+// Testing Route
+router.get("/home", async (req, res) => {
+  return res.render("home");
+});
+// Testing Route
+router.get("/aboutus", async (req, res) => {
+  return res.render("aboutus");
+});
+// Testing Route
 router.get("/login", async (req, res) => {
-    return res.render("login")
+  return res.render("login");
 });
 
+// Testing Route
+router.get("/signup", async (req, res) => {
+  return res.render("signup");
+});
+
+// Testing Route
+router.get("/news", async (req, res) => {
+    return res.render("news");
+  });
+  
 module.exports = router;
