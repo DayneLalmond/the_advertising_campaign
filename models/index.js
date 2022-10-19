2a64579e775bd6ce5b68edc9533220589f997864
Dayne Lalmond <dlalmondo@gmail.com>
2022-10-18 21:50:18 -0400

    model index fix

--- models/index.js
+++ models/index.js
@@ -3,10 +3,12 @@ const Ad = require('./ad');
 
 User.hasMany(Ad, {
     foreignKey: 'user_id',
+    onDelete: 'CASCADE'
 });
   
 Ad.belongsTo(User, {
     foreignKey: 'user_id',
+    onDelete: 'CASCADE'
 });
 
 module.exports = { User, Ad };
